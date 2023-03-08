d24fc06e2f32b277a4b76791cd4e7ee3bd1e8042
Jinwook Kim <jinwook31@naver.com>
2023-03-08 19:21:11 +0900

    AR Modification
    Augment different virtual object depending on the tag id.

--- Assets/DetectionTest.cs
+++ Assets/DetectionTest.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Linq;
 using UI = UnityEngine.UI;
 using Klak.TestTools;
+using System.Collections.Generic;
 
 sealed class DetectionTest : MonoBehaviour
 {
@@ -9,17 +10,25 @@ sealed class DetectionTest : MonoBehaviour
     [SerializeField] int _decimation = 4;
     [SerializeField] float _tagSize = 0.05f;
     [SerializeField] Material _tagMaterial = null;
+    [SerializeField] GameObject[] _prefab = null;
     [SerializeField] UI.RawImage _webcamPreview = null;
     [SerializeField] UI.Text _debugText = null;
+    [SerializeField] int[] idList = {2099, 2107, 2108, 2109, 2113};
 
     AprilTag.TagDetector _detector;
     TagDrawer _drawer;
+
+    private List<int> tagLog;
+    private List<bool> tagStatus;
 
     void Start()
     {
         var dims = _source.OutputResolution;
         _detector = new AprilTag.TagDetector(dims.x, dims.y, _decimation);
-        _drawer = new TagDrawer(_tagMaterial);
+        _drawer = new TagDrawer(_tagMaterial, _prefab, idList);
+        
+        tagLog = new List<int>();
+        tagStatus = new List<bool>();
     }
 
     void OnDestroy()
@@ -40,9 +49,31 @@ sealed class DetectionTest : MonoBehaviour
         var fov = Camera.main.fieldOfView * Mathf.Deg2Rad;
         _detector.ProcessImage(image, fov, _tagSize);
 
+        for(int i=0; i < tagStatus.Count; i++){
+            tagStatus[i] = false;
+        }
+
         // Detected tag visualization
-        foreach (var tag in _detector.DetectedTags)
-            _drawer.Draw(tag.ID, tag.Position, tag.Rotation, _tagSize);
+        foreach (var tag in _detector.DetectedTags){
+            // Add new found tag
+            if(!tagLog.Contains(tag.ID)){
+                Debug.Log(tag.ID);
+                tagLog.Add(tag.ID);
+                tagStatus.Add(true);
+                _drawer.Draw(tag.ID, tag.Position, tag.Rotation, _tagSize);
+
+            }else{
+                tagStatus[tagLog.IndexOf(tag.ID)] = true;
+                _drawer.Draw(tag.ID, tag.Position, tag.Rotation, _tagSize);
+            }
+        }
+
+        // remove augmented object if tag is gone
+        for(int i=0; i<tagStatus.Count; i++){
+            if(!tagStatus[i])
+                _drawer.unDraw(tagLog[i]);
+        }  
+
 
         // Profile data output (with 30 frame interval)
         if (Time.frameCount % 30 == 0)
